7eb1e44f9b8c46e68493b34d348ce4a48726342b
CaetanoPF <caetanopfreitas@gmail.com>
2025-11-21 14:46:33 -0300

    FEAT: Rotas do index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,8 +12,33 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
+    },
+    {
+      path: '/acervototal',
+      name: 'acervototal',
+      component: () => import('../views/AcervoView.vue'),
+    },
+    {
+      path: '/sidefilter',
+      name: 'sidefilter',
+      component: () => import('../components/SideFilter.vue'),
+    },
+    {
+      path: '/acervocard',
+      name: 'acervocard',
+      component: () => import('../components/AcervoCard.vue'),
+    },
+    {
+      path: '/navbar',
+      name: 'navbar',
+      component: () => import('../components/NavBar.vue'),
     }
   ],
+
+scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
+    return { top: 0 };
+  }
 })
 
 export default router
